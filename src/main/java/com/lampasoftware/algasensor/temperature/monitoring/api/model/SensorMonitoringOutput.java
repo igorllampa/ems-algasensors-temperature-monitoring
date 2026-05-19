bed590667a588d666274264073a95d4cebad3f95
igorl <igorllampa@gmail.com>
2026-05-19 21:12:46 +0100

    feat: add model for sensor monitoring and repository

---
+++ src/main/java/com/lampasoftware/algasensor/temperature/monitoring/api/model/SensorMonitoringOutput.java
@@ -0,0 +1,17 @@
+package com.lampasoftware.algasensor.temperature.monitoring.api.model;
+
+import com.lampasoftware.algasensor.temperature.monitoring.domain.model.SensorId;
+import io.hypersistence.tsid.TSID;
+import lombok.Builder;
+import lombok.Data;
+
+import java.time.OffsetDateTime;
+
+@Data
+@Builder
+public class SensorMonitoringOutput {
+    private TSID id;
+    private Double lastTemperature;
+    private OffsetDateTime updatedAt;
+    private Boolean enabled;
+}

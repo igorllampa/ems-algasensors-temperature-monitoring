b1234ed4d0adc7d1b7d0a6babd9428328549a66c
igorl <igorllampa@gmail.com>
2026-08-20 08:51:28 +0100

    refactor: change temperature api model name and serializer/deserializer name to be more intuitive

---
+++ src/main/java/com/lampasoftware/algasensor/temperature/monitoring/api/config/jackson/StringToTSIDDeserializer.java
@@ -0,0 +1,19 @@
+package com.lampasoftware.algasensor.temperature.monitoring.api.config.jackson;
+
+import com.fasterxml.jackson.core.JacksonException;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import io.hypersistence.tsid.TSID;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class StringToTSIDDeserializer extends JsonDeserializer<TSID> {
+
+    @Override
+    public TSID deserialize(JsonParser jsonParser, DeserializationContext deserializationContext) throws IOException, JacksonException {
+        return TSID.from(jsonParser.getText());
+    }
+}

24274a20f180ea6186b1a11e2436d953c132f0b5
igorl <igorllampa@gmail.com>
2026-09-22 21:49:42 +0100

    refactor: remove code used to force an error and test the deadletterqueue

--- src/main/java/com/lampasoftware/algasensor/temperature/monitoring/domain/service/TemperatureMonitoringService.java
+++ src/main/java/com/lampasoftware/algasensor/temperature/monitoring/domain/service/TemperatureMonitoringService.java
@@ -25,9 +25,9 @@ public class TemperatureMonitoringService {
     @Transactional
     public void processTemperatureReading(TemperatureLogData temperatureLogData){
 
-        if (temperatureLogData.getValue().equals(10.5)) {
-            throw new RuntimeException("Test error");
-        }
+//        if (temperatureLogData.getValue().equals(10.5)) {
+//            throw new RuntimeException("Test error");
+//        }
         sensorMonitoringRepository.findById(new SensorId(temperatureLogData.getSensorId()))
                 .ifPresentOrElse(sensor -> handleSensorMonitoring(temperatureLogData, sensor),
                         () -> logIgnoredTemperature(temperatureLogData));
